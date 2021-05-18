c529b0ec4dffe0108d290b34774b10ae84c11cff
cmcla003 <51941943+cmcla003@users.noreply.github.com>
2021-05-19 10:20:23 +1200

    Inital set up base component

---
+++ 01_base_compoenent_V1.py
@@ -0,0 +1,27 @@
+# import statements
+
+# functions go here
+
+# *** Main Routine ***
+
+# Set up dictionaries and lists to hold data.
+
+# Ask user if they have used the program before and show instructions if needed
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # Ask for payment method (apply surcharge if applicable)
+
+# Calculate total sales & profit
+
+# Output data to text file
