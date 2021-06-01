31965c50ce2b1d85b01d2d6d335c8387772ba7fd
cmcla003 <mclarenc@awatapu.school.nz>
2021-06-02 11:52:02 +1200

    ticket loop added to base

--- 01_base_compoenent_V1.py
+++ 01_base_compoenent_V1.py
@@ -24,9 +24,30 @@ def not_blank(question):
 # Ask user if they have used the program before and show instructions if needed
 
 # Loop to get ticket details
+# intialise variables
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+    # tells user how many tickets left
+    if count < 4:
+        print("You have {} places left".format(MAX_TICKETS - count))
+    #  on place left
+    else:
+        print("***You have ONE place left***")
 
     # Get name (can't be blank)
     name = not_blank("Name: ")
+    count += 1
+    print()
+
+if count == MAX_TICKETS:
+    print("You have sold all available tickets.")
+else:
+    print("You have sold {} tickets. \n"
+          "There are {} tickets available.".format(count, MAX_TICKETS-count))
+
 
     # Get age (between 12 and 130)
 
