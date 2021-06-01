c4fcb505d0222384ee6c2f3e1a22ff8c5b566bbc
cmcla003 <mclarenc@awatapu.school.nz>
2021-06-02 10:49:01 +1200

    ticket loop adjusted < not <=

---
+++ 02_ticket_loop.py
@@ -0,0 +1,12 @@
+# start of loop
+
+# intialise variables
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+
+    # get details
+    name = input("Name: ")
+    count +=1
