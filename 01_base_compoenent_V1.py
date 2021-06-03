70f4715b13ecb7a579e7b26858c2116970f694d8
cmcla003 <mclarenc@awatapu.school.nz>
2021-06-04 08:12:43 +1200

    ticket price calculation added: variable names changed

--- 01_base_compoenent_V1.py
+++ 01_base_compoenent_V1.py
@@ -44,14 +44,16 @@ def int_check (question):
 
 # Loop to get ticket details
 # intialise variables
-name = ""
-count = 0
-MAX_TICKETS = 5
 
-while name != "xxx" and count < MAX_TICKETS:
+MAX_TICKETS = 5
+name = ""
+ticket_count = 0
+ticket_sales = 0
+
+while name != "xxx" and ticket_count < MAX_TICKETS:
     # tells user how many tickets left
-    if count < 4:
-        print("You have {} places left".format(MAX_TICKETS - count))
+    if ticket_count < MAX_TICKETS-1:
+        print("You have {} places left".format(MAX_TICKETS - ticket_count))
     #  on place left
     else:
         print("***You have ONE place left***")
@@ -65,6 +67,8 @@ while name != "xxx" and count < MAX_TICKETS:
     # Get age (between 12 and 130)
     age = int_check("Age: ")
 
+
+    # check age of person
     if age <12:
         print("Sorry you are too young for this movie")
         continue
@@ -72,16 +76,29 @@ while name != "xxx" and count < MAX_TICKETS:
         print("That is very old, it looks like a mistake.")
         continue
 
-    count += 1
+    # adjust ticekt price based on age
+    if age < 16:
+        ticket_price = 7.5
 
-if count == MAX_TICKETS:
+    elif age < 65:
+        ticket_price = 10.5
+
+    else:
+        ticket_price = 6.5
+
+    ticket_count += 1
+    ticket_sales += ticket_price
+
+ # Calculate ticket price & profit
+ticket_profit = (ticket_sales - (5 * ticket_count))
+print("Ticket Profit: ${:.2f}". format(ticket_profit))
+
+if ticket_count == MAX_TICKETS:
     print("You have sold all available tickets.")
 else:
     print("You have sold {} tickets. \n"
-          "There are {} tickets available.".format(count, MAX_TICKETS-count))
+          "There are {} tickets available.".format(ticket_count, MAX_TICKETS-ticket_count))
 
-
-    # Calculate ticket price
 
     # Loop to ask for snacks
 
